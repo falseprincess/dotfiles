3a0cc2a55267e9bff85075327230ee6757f5fb44
falseprincess <66030663+falseprincess@users.noreply.github.com>
2020-12-30 23:09:22 +0000

    Add files via upload

--- dmenu/config.h
+++ dmenu/config.h
@@ -4,15 +4,17 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"terminus:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeHp] = { "#bbbbbb", "#333333" }
+	[SchemeSel] = { "#A3BE8C", "#005577" },
+	[SchemeSelHighlight] = { "#A3BE8C", "#4C566A" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeOut] = { "#A3BE8C", "#00ffff" },
+	[SchemeHp] = { "#A3BE8C", "#333333" }
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
