c2dd36e92530f8ba6f5baddfc5d5a70f16242dc4
falseprincess <66030663+falseprincess@users.noreply.github.com>
2021-05-22 20:53:03 -0600

    Add files via upload
    Adjusted some colors to fit with my overall dwm theme.

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -4,19 +4,19 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"dina:size=10"
+        "GohuFont:style=Regular:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ebdbb2", "#1D2021" },
-	[SchemeSel] = { "#1D2021", "#8EC07C" },
+	[SchemeSel] = { "#1D2021", "#b16286" },
 	[SchemeOut] = { "#ebdbb2", "#8ec07c" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 18;
+static unsigned int lineheight = 10;
 static unsigned int min_lineheight = 8;
 
 /*
