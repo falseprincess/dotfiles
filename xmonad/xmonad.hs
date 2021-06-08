60e6627d33be173947fdd2b96465722d22a13fae
falseprincess <66030663+falseprincess@users.noreply.github.com>
2021-06-08 16:35:41 -0600

    Add files via upload

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,8 +1,5 @@
 
-
--- IMPORTS
-
--- base
+{-- Imports --}
 import XMonad
 import XMonad.Config.Desktop
 import qualified XMonad.StackSet as W
@@ -30,9 +27,11 @@ import XMonad.Hooks.ManageHelpers (isFullscreen, isDialog,  doFullFloat, doCente
 
 -- layout
 import XMonad.Layout.Renamed (renamed, Rename(Replace))
-import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing
-import XMonad.Layout.ResizableTile
+import XMonad.Layout.LimitWindows
+import XMonad.Layout.SimplestFloat
+import XMonad.Layout.LayoutModifier
+import XMonad.Layout.WindowArranger
 
 -- system
 import System.Exit (exitSuccess)
@@ -41,30 +40,54 @@ import System.IO (hPutStrLn)
 {-- Autostart --}
 myStartupHook :: X ()
 myStartupHook = do
-    spawnOnce "sh -c  '~/.fehbg'"                          -- setting my wallpaper.
-    spawnOnce "xsetroot -cursor_name left_ptr &"           -- setting my cursor theme.
-    spawnOnce "xcompmgr -c -C -t-5 -l-5 -r4.2 -o.55 &"     -- starting my compositor.
+    spawnOnce "sh -c  '~/.fehbg' &"                                    -- setting my wallpaper.
+    spawnOnce "xsetroot -cursor_name left_ptr &"                       -- setting my cursor theme.
+    spawnOnce "xcompmgr -c -C -t-5 -l-5 -r4.2 -o.55 &"                 -- starting my compositor.
 
 {-- Keybindings --}
+myKeys :: XConfig Layout -> M.Map (ButtonMask,KeySym) (X ())
 myKeys = \c -> mkKeymap c $
-    [ ("M-S-c",        kill)
-    , ("M-C-e",        spawn "emacs")
-    , ("M-S-<Return>", spawn $ terminal c)
+    [ ("M-p",          spawn "dmenu_run -p 'RUN ->' -h 18")            -- launch dmenu.
+    , ("M-S-<Return>", spawn $ terminal c)                             -- launch my terminal.
     , ("M-C-d",        spawn "discord")
+    , ("M-C-p",        spawn "pavucontrol")
     , ("M-C-b",        spawn "firefox-bin -p")
-    , ("<F6>",         spawn "xbacklight -inc 10")                      -- brightness.
-    , ("<F5>",         spawn "xbacklight -dec 10")
-    , ("<F3>",         spawn "amixer set Master 5%+")                   -- audio.
-    , ("<F2>",         spawn "amixer set Master 5%-")
-    , ("<F1>",         spawn "amixer set Master toggle")
-    , ("<Print>",      spawn "gnome-screenshot -i")                     -- quit Xmonad.
-    , ("M-<Space>",    sendMessage NextLayout)                             -- select layout
-    , ("M-S-q",        io exitSuccess)                                  -- quit Xmonad.
-    , ("M-S-r",        spawn "xmonad --recompile; xmonad --restart") ]  -- recompile and restart Xmonad.
 
+    -- emacs.
+    , ("M-C-e",        spawn "emacs")
+
+    -- brightness.
+    , ("<F6>",         spawn "xbacklight -inc 10")                     -- brightness up.
+    , ("<F5>",         spawn "xbacklight -dec 10")                     -- brightness down.
+
+    -- audio.
+    , ("<F3>",         spawn "amixer set Master 5%+")                  -- audio up.
+    , ("<F2>",         spawn "amixer set Master 5%-")                  -- audio down.
+    , ("<F1>",         spawn "amixer set Master toggle")               -- toggle mute.
+
+    -- screenshot.
+    , ("<Print>",      spawn "gnome-screenshot -i")
+
+    -- layouts.
+    , ("M-<Space>",    sendMessage NextLayout)                         -- cycle through my layouts.
+    , ("M-S-q",        io exitSuccess)                                 -- quit Xmonad.
+    , ("M-S-r",        spawn "xmonad --recompile; xmonad --restart")   -- recompile and restart Xmonad.
+
+    -- killing windows.
+    , ("M-S-a",        killAll)                                        -- kill all windows in this workspace.
+    , ("M-S-t",        sinkAll)                                        -- retile all floating windows.
+    , ("M-S-c",        kill)                                           -- kill the focused window.
+    
+    -- increase/decrease spacing (gaps)
+    , ("C-M1-j",       decWindowSpacing 4)                             -- decrease window spacing.
+    , ("C-M1-k",       incWindowSpacing 4)                             -- increase window spacing.
+    , ("C-M1-h",       decScreenSpacing 4)                             -- decrease screen spacing.
+    , ("C-M1-l",       incScreenSpacing 4)                             -- increase screen spacing.
+    ]
+    
 {-- Managing window rules --}
 myManageHook = composeAll
-  [ className =? "notification"              --> doFloat
+  [ className =? "notification"              --> doFloat               -- what clients should float?
   , className =? "file_progress"             --> doFloat
   , className =? "toolbar"                   --> doFloat
   , className =? "confirm"                   --> doFloat
@@ -74,56 +97,63 @@ myManageHook = composeAll
   , className =? "discord"                   --> doFloat
   , className =? "Pavucontrol"               --> doFloat
   , className =? "Slippi-r18-netplay"        --> doFloat
+  , title =? "Firefox - Choose User Profile" --> doFloat
   , title =? "Oracle VM VirtualBox Manager"  --> doFloat
-  , (className =? "firefox" <&&> resource =? "Dialog") --> doFloat ]  -- Float Firefox Dialog.
+
+  -- pushing specified clients to said workspace.
+  , title =? "Mozilla Firefox"               --> doShift ( myWorkspaces !! 2 )
+  , className =? "Gimp"                      --> doShift ( myWorkspaces !! 5 )
+  , className =? "discord"                   --> doShift ( myWorkspaces !! 3 )
+  , className =? "VirtualBox Manager"        --> doShift ( myWorkspaces !! 4 )
+  ]
 
 {-- Defining my workspaces --}
-myWorkspaces = [ "term","emacs","www","chat","gfx","virt","misc" ]
+myWorkspaces = [ "term","emacs","www","chat","vbox","gfx","office","misc" ]
 
--- layout
-myLayout = avoidStruts (tiled ||| full)
-    where
-        -- full
-        full = renamed [Replace "Full"]
-                $ noBorders (Full)
-        -- tiled
-        tiled = renamed [Replace "Tall"]
-                $ spacingRaw True (Border 10 0 10 0) True (Border 0 10 0 10) True
-                $ ResizableTall 1 (3/100) (1/2) []
-        -- default number of windows in master pane
-        nmaster = 1
-        -- default proportion of screen occupied by master pane
-        ratio = 1/2
-        -- percent of screen to increment by when resizing panes
-        delta = 3/100
+{-- Layouts --}
+mySpacing :: Integer -> l a -> ModifiedLayout Spacing l a
+mySpacing i = spacingRaw False (Border 0 i 0 i) True (Border i 0 i 0) True
+
+tall =
+  renamed [Replace "Tall"] $ mySpacing 10 $ Tall 1 (3 / 100) (1 / 2)
+full =
+  renamed [Replace "Full"] $ mySpacing 0 $ limitWindows 15 Full
+
+
+
+myLayout =
+  avoidStruts $ myDefaultLayout
+  where
+    myDefaultLayout = tall ||| full ||| simplestFloat 
 
 {-- All of my important stuff --}
 main :: IO ()
 main = do
   xmproc <- spawnPipe "xmobar ~/.xmobar.hs"
   xmonad $ ewmh desktopConfig
-        { manageHook = ( isFullscreen --> doFullFloat ) <+> myManageHook <+> manageHook desktopConfig <+> manageDocks
-      , layoutHook  = smartBorders $ myLayout
-      , logHook     = dynamicLogWithPP xmobarPP    -- managing pretty printing.
-      { ppOutput    = hPutStrLn xmproc
-      , ppVisible   = xmobarColor "#81a1c1" ""
-      , ppHidden    = xmobarColor "#81a1c1" "" . wrap "*" ""
-      , ppHiddenNoWindows     = xmobarColor "#4c566a" "" 
-      , ppCurrent   = xmobarColor "#d8dee9" "" . wrap "[" "]"
-      , ppUrgent    = xmobarColor "#bf616a" "" . wrap "!" "!"
-      , ppTitle     = xmobarColor "#8fbcbb" "" . shorten 30
-      , ppSep       = " | "
-      , ppWsSep     = "  "
-      , ppOrder     = \(ws:l:t:ex) -> [ws,l]++ex++[t]
+    { manageHook                = myManageHook <+> manageDocks
+      , handleEventHook         = docksEventHook
+      , layoutHook              = myLayout
+      , logHook                 = dynamicLogWithPP xmobarPP           -- managing pretty printing.
+      { ppOutput                = hPutStrLn xmproc
+      , ppVisible               = xmobarColor "#81a1c1" ""
+      , ppHidden                = xmobarColor "#81a1c1" "" . wrap "*" ""
+      , ppHiddenNoWindows       = xmobarColor "#4c566a" "" 
+      , ppCurrent               = xmobarColor "#d8dee9" "" . wrap "[" "]"
+      , ppUrgent                = xmobarColor "#bf616a" "" . wrap "!" "!"
+      , ppTitle                 = xmobarColor "#a3be8c" "" . shorten 40
+      , ppSep                   = "<fc=#d8dee9> <fn=1>|</fn> </fc>"
+      , ppWsSep                 = "  "
+      , ppOrder                 = \(ws:l:t:ex) -> [ws,l]++ex++[t]
       }
     -- , layoutHook             = myLayout
-    , terminal               = "st"                -- setting my terminal.
-    , modMask                = mod4Mask            -- sebind Mod to the Windows key.
-    , borderWidth            = 1                   -- border width.
-    , normalBorderColor      = "#4c566a"           -- unselected border color.
-    , focusedBorderColor     = "#d8dee9"           -- selected border color.
-    , focusFollowsMouse      = True                -- focus follows mouse T/F.
-    , workspaces             = myWorkspaces        -- defining my workspaces.
-    , startupHook            = myStartupHook       -- autostarting some programs.
-    , keys                   = myKeys <+> keys def -- defining mykeys for keybindings.
-    } `additionalKeysP` []                         -- for those easy emacs style keybindings. 
+    , terminal               = "st"                                   -- setting my terminal.
+    , modMask                = mod4Mask                               -- sebind Mod to the Windows key.
+    , borderWidth            = 1                                      -- border width.
+    , normalBorderColor      = "#4c566a"                              -- unselected border color.
+    , focusedBorderColor     = "#d8dee9"                              -- selected border color.
+    , focusFollowsMouse      = True                                   -- focus follows mouse.
+    , workspaces             = myWorkspaces                           -- defining my workspaces.
+    , startupHook            = myStartupHook                          -- autostarting some programs.
+    , keys                   = myKeys <+> keys def                    -- defining mykeys for keybindings.
+    } `additionalKeysP` []                        
