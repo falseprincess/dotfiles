4fc031efda7374956fa0209e1a105e77ea9a3765
falseprincess <66030663+falseprincess@users.noreply.github.com>
2021-06-08 16:38:48 -0600

    Add files via upload

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -4,20 +4,21 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-        "GohuFont:style=Regular:size=14"
+	"GohuFont:style=Regular:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ebdbb2", "#1D2021" },
-	[SchemeSel] = { "#1D2021", "#b16286" },
-	[SchemeOut] = { "#ebdbb2", "#8ec07c" },
+	[SchemeNorm] = { "#d8dee9", "#2e3440" },
+	[SchemeSel] = { "#2e3440", "#88c0d0" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeHp] = { "#bbbbbb", "#333333" }
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 10;
-static unsigned int min_lineheight = 8;
+static unsigned int lineheight = 24;
+static unsigned int min_lineheight = 24;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -26,4 +27,4 @@ static unsigned int min_lineheight = 8;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 2;
+static unsigned int border_width = 1;
