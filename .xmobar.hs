c41c62174bac50e574e2b390b85fef3fb2475983
falseprincess <66030663+falseprincess@users.noreply.github.com>
2021-06-08 16:36:23 -0600

    Add files via upload

--- .xmobar.hs
+++ .xmobar.hs
@@ -12,7 +12,7 @@ Config {
    -- layout
    , sepChar =  "%"   -- delineator between plugin names and straight text
    , alignSep = "}{"  -- separator between left-right alignment
-   , template = " %StdinReader% }{ %battery% - %date% "
+   , template = " %StdinReader% }{ %dynnetwork% | %alsa:default:Master%| %battery% | %date% "
 
    -- general behavior
    , lowerOnStart =     True    -- send to bottom of window stack on start
@@ -33,6 +33,16 @@ Config {
                              , "--high"     , "#a3be8c"
                              ] 10
 
+        , Run Alsa "default" "Master" [ "-t", "Vol: <volume>% <status>"
+                                      , "-f", "▊", "-b", "─"
+                                      , "-H", "70", "-h", "#bf616a"
+                                      , "-L", "21", "-l", "#a3be8c"
+
+                                      , "--" --
+                                                , "-c", "#bf616a"
+                                                , "-C", "#a3be8c"
+                                      ] 
+
         -- battery monitor
         , Run Battery        [ "--template" , "Batt: <acstatus>"
                              , "--Low"      , "10"        -- units: %
@@ -45,13 +55,25 @@ Config {
                                        -- discharging status
                                        , "-o"	, "<left>% (<timeleft>)"
                                        -- AC "on" status
-                                       , "-O"	, "<fc=#a3be8c>Charging</fc>"
+                                       , "-O"	, "<fc=#a3be8c><left>% +</fc>"
                                        -- charged status
-                                       , "-i"	, "<fc=#a3be8c>Charged</fc>"
+                                       , "-i"	, "<fc=#a3be8c><left>% (full)</fc>"
                              ] 50 
+
+        -- network activity monitor (dynamic interface resolution)
+        , Run DynNetwork [ "--template" , "<dev>: <rx>KB - <tx>KB"
+                         , "-S"         , "True"
+                         , "--Low"      , "1000"       -- units: B/s
+                         , "--High"     , "5000"       -- units: B/s
+                         , "--low"      , "#a3be8c"
+                         , "--normal"   , "#a3be8c"
+                         , "--high"     , "#bf616a"
+
+                         , "--", "--devices", "mullvad-134"
+                         ] 10
           
         -- time and date indicator 
-        , Run Date           "<fc=#d8dee9>%F (%a) %T</fc>" "date" 10
+        , Run Date           "<fc=#d8dee9>%F (%a) %r</fc>" "date" 10
 
         -- pipe my workspaces into the bar.
 	, Run StdinReader
